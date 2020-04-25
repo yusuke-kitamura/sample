f193723b4dbdcdf7c3866f0a38ae7ed146fa579f
vagrant <vagrant@localhost.localdomain>
2020-04-25 05:47:39 +0000

    ユーザー登録時のエラーメッセージ追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,4 @@
+class User < ApplicationRecord
   before_save { self.email = email.downcase }
   validates :name, presence: true, length: { maximum: 50 }
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
@@ -5,6 +6,5 @@
                     format: { with: VALID_EMAIL_REGEX },
                     uniqueness: { case_sensitive: false }
   has_secure_password
-
-  validates :password, presence: true, length: {minimum: 6}
+  validates :password, presence: true, length: { minimum: 6 }
 end
